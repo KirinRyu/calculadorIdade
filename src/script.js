0019e587a697c517a9d530eb686d8aba028f18f0
Ryu Satoy <dan_ryuichi@outlook.com>
2024-02-29 18:05:50 -0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,31 @@
+function clicado() {
+    var data_atual = new Date()
+    var ano_atual = data_atual.getFullYear()
+    var res = window.document.getElementById("result")
+    var ano_inserido = Number(window.document.getElementById("data").value)
+    var botao_sexo = window.document.getElementsByName("sexo")
+    var botao_ecolha = window.document.getElementsByName("aniversario")
+    
+    var idade = ano_atual - ano_inserido
+    
+    if(botao_ecolha[0].checked==false)
+    {
+        idade--
+    }
+    if(idade<=0)
+    {
+        res.innerHTML="<strong>[ERRO]<strong> VALOR INVÁLIDO"
+        return
+    }
+    if(botao_sexo[0].checked==true)
+    {
+        var sex = "um homem"
+    }else{
+        var sex = "uma mulher"
+    }
+    res.innerText=`Você é ${sex} de ${idade} anos`
+}
+
+function reset() {
+    location.reload()
+}
